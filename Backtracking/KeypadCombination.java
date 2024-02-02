7ec716a9013d03b44593d61206d240b11cc67449
Nishu Singh <53341807+nishusingh71@users.noreply.github.com>
2024-02-02 13:28:34 +0000

    update

--- Backtracking/KeypadCombination.java
+++ Backtracking/KeypadCombination.java
@@ -1,5 +1,30 @@
 package Backtracking;
 
 public class KeypadCombination {
-    // final static char[][]={}
+    final static char[][] L = { {}, {}, { 'a', 'b', 'c' }, { 'd', 'e', 'f' }, { 'g', 'h', 'i' }, { 'j', 'k', 'l' },
+            { 'm', 'n', 'o' }, { 'p', 'q', 'r', 's' }, { 't', 'u', 'v' }, { 'w', 'x', 'y', 'z' } };
+
+    public static void letterCombo(String D) {
+        int len = D.length();
+        if (len == 0) {
+            System.out.println("");
+            return;
+        }
+        bfs(0, len, new StringBuilder(), D);
+    }
+
+    private static void bfs(int pos, int len, StringBuilder sb, String D) {
+        if (pos == len) {
+            System.out.print(sb.toString());
+        } else {
+            char[] letters = L[Character.getNumericValue(D.charAt(pos))];
+            for (int i = 0; i < letters.length; i++) {
+                bfs(pos + 1, len, new StringBuilder(sb).append(letters[i]), D);
+            }
+        }
+       
+    }
+    public static void main(String[] args) {
+        letterCombo("9");
+    }
 }
