351839ed3c80b868f7c13d54de2e77a87b2e3fa2
Nishu Singh <53341807+nishusingh71@users.noreply.github.com>
2024-02-22 15:03:01 +0000

    update

--- BinaryTree/BinaryTreeB.java
+++ BinaryTree/BinaryTreeB.java
@@ -1,5 +1,6 @@
 package BinaryTree;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -229,6 +230,99 @@ public class BinaryTreeB {
             }
             System.out.println();
         }
+
+        // Kth level
+        public static void Klevel(Node root, int level, int k) {
+            if (root == null) {
+                return;
+            }
+            if (level == k) {
+                System.out.println(root.data + " ");
+                return;
+            }
+            Klevel(root.left, level + 1, k);
+            Klevel(root.right, level + 1, k);
+        }
+
+        // Lowest Common Ancestor-Approch-1
+        public static Node lca(Node root, int n1, int n2) {
+            ArrayList<Node> path1 = new ArrayList<>();
+            ArrayList<Node> path2 = new ArrayList<>();
+            getPath(root, n1, path1);
+            getPath(root, n2, path2);
+            // last common ancestor
+            int i = 0;
+            for (; i < path1.size() & i < path2.size(); i++) {
+                if (path1.get(i) != path2.get(i)) {
+                    break;
+                }
+            }
+            // last equal node ->i-th
+            Node lca = path1.get(i - 1);
+            return lca;
+        }
+
+        public static boolean getPath(Node root, int n, ArrayList<Node> path) {
+            if (root == null)
+                return false;
+            path.add(root);
+            if (root.data == n) {
+                return true;
+            }
+            boolean foundLeft = getPath(root.left, n, path);
+            boolean foundRight = getPath(root.right, n, path);
+            if (foundLeft || foundRight) {
+                return true;
+            }
+            path.remove(path.size() - 1);
+            return false;
+        }
+
+        // LCA 2
+        public static Node lca2(Node root, int n1, int n2) {
+            if (root == null)
+                return null;
+            if (root.data == n1 || root.data == n2) {
+                return root;
+            }
+            Node leftLca = lca2(root.left, n1, n2);
+            Node rightLca = lca2(root.right, n1, n2);
+            // leftLca=val rightLca=null
+            if (rightLca == null) {
+                return leftLca;
+            }
+            if (leftLca == null) {
+                return rightLca;
+            }
+            return root;
+
+        }
+
+        // Min distance b/w nodes
+        public static int minDist(Node root, int n1, int n2) {
+            Node lca = lca2(root, n1, n2);
+            int dist1 = lcaDist(lca, n1);
+            int dist2 = lcaDist(lca, n2);
+            return dist1 + dist2;
+        }
+
+        public static int lcaDist(Node root, int n) {
+            if (root == null) {
+                return -1;
+            }
+            if (root.data == n) {
+                return 0;
+            }
+            int leftDist = lcaDist(root.left, n);
+            int rightDist = lcaDist(root.right, n);
+            if (leftDist == -1 && rightDist == -1)
+                return 1;
+            else if (leftDist == -1)
+                return rightDist + 1;
+            else {
+                return leftDist + 1;
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -256,12 +350,18 @@ public class BinaryTreeB {
         root.right = new Node(3);
         root.right.right = new Node(6);
         // subRoot
-        Node subRoot = new Node(2);
-        subRoot.left = new Node(4);
+        // Node subRoot = new Node(2);
+        // subRoot.left = new Node(4);
         // subRoot.right = new Node(5);
-        System.out.println(BinaryTree.isSubtree(root, subRoot));
+        // System.out.println(BinaryTree.isSubtree(root, subRoot));
         // System.out.println(topView(subRoot));
-        BinaryTree.topView(root);
+        // BinaryTree.topView(root);
+        // int k = 3;
+        // BinaryTree.Klevel(root, 3, k);
+        int n1 = 4, n2 = 5;
+        // System.out.println( BinaryTree.lca(root, n1, n2));
+        // System.out.println(BinaryTree.lca2(root, n1, n2).data);
+        System.out.println(BinaryTree.minDist(root, n1, n2));
 
     }
 }
