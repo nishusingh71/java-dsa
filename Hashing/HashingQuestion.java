ab7df286f04c5b79c9b67d455a308ef122855a56
Nishu Singh <53341807+nishusingh71@users.noreply.github.com>
2024-03-04 12:02:06 +0000

    up

--- Hashing/HashingQuestion.java
+++ Hashing/HashingQuestion.java
@@ -43,6 +43,86 @@ public class HashingQuestion {
         return res.toString();
     }
 
+    class Node {
+        int key;
+        int value;
+        Node next;
+        Node prev;
+
+        Node() {
+
+        }
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+            next = null;
+            prev = null;
+        }
+
+    }
+
+    Node head;
+    Node tail;
+    int maxCapcity;
+    HashMap<Integer, Node> map;
+
+    public void LRUCahe(int capcity) {
+        head = new Node();
+        head.next = tail;
+        tail.prev = head;
+        this.maxCapcity = capcity;
+        map = new HashMap<>();
+
+    }
+
+    public void addLast(Node node) {
+        Node temp = tail.prev;
+        node.next = tail;
+        tail.prev = node;
+        node.prev = temp;
+    }
+
+    public void remove(Node node) {
+        Node prevNode = node.prev;
+        Node nextNode = node.next;
+        prevNode.next = nextNode;
+        prevNode.prev = prevNode;
+        node.prev = null;
+        node.next = null;
+    }
+
+    public void moveToLast(Node node) {
+        remove(node);
+        addLast(node);
+    }
+
+    public int getKey(int key) {
+        if (map.containsKey(key) == false) {
+            return -1;
+        } else {
+            Node node = map.get(key);
+            int val = node.value;
+            moveToLast(node);
+            return val;
+        }
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key) == false) {
+            Node node = new Node(key, value);
+            if (map.size() == maxCapcity) {
+                Node LRU_node = head.next;
+                remove(LRU_node);
+                map.remove(LRU_node.key);
+            } else {
+                node = map.get(key);
+                node.value = value;
+                moveToLast(node);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         // int arr[] = { 2, 7, 11, 15 };
         // int target = 9;
