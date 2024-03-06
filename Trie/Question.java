d90bc13e936ec41674501e092e4f7f1ca503f622
Nishu Singh <53341807+nishusingh71@users.noreply.github.com>
2024-03-06 04:41:09 +0000

    updates

--- Trie/Question.java
+++ Trie/Question.java
@@ -53,18 +53,76 @@ public class Question {
             }
         }
     }
-   //Longest word in Dictionary
-   private static class Node{
-    private char data;
-    private String words;
-    private boolean is;
 
-   }
+    // Longest word in Dictionary
+    private static class Node {
+        private char data;
+        private String words;
+        private boolean isEnd;
+        private Node[] children;
+
+        public Node(char data) {
+            this.data = data;
+            this.data = data;
+            this.words = null;
+            this.isEnd = false;
+            this.children = new Node[26];
+        }
+
+    }
+
+    private static Node roots = new Node('/');
+    private static String answer = " ";
+
+    private static void insert(String word) {
+        Node curr = roots;
+        for (int i = 0; i < word.length(); i++) {
+            int childIdx = word.charAt(i) - 'a';
+            if (curr.children[childIdx] == null) {
+                curr.children[childIdx] = new Node(word.charAt(i));
+            }
+            curr = curr.children[childIdx];
+        }
+        curr.isEnd = true;
+        curr.words = word;
+    }
+
+    private static void dfs(Node node) {
+        if (node == null) {
+            return;
+        }
+        if (node.words != null) {
+            if (node.words.length() > answer.length()) {
+                answer = node.words;
+            } else if (node.words.length() == answer.length() && node.words.compareTo(answer) < 0) {
+                answer = node.words;
+            }
+        }
+        for (Node child : node.children) {
+            if (child != null && child.words != null) {
+                dfs(node);
+            }
+        }
+    }
+
+    public static String longestWord(String[] words) {
+        for (String word : words) {
+            insert(word);
+        }
+        Node curr = roots;
+        dfs(curr);
+        return answer;
+    }
+
     public static void main(String[] args) {
-        String word[] = { "eat", "tea", "titan", "ate", "mat", "bat" };
+        String word[] = { "w", "wo", "wor", "worl", "world" };
         for (int i = 0; i < word.length; i++) {
-            build(word[i]);
+            insert(word[i]);
+            // System.out.println(longestWord(new String[i]));
         }
         // System.out.println(groupAnagrams(""));
+        // for (int i = 0; i < word.length; i++)
+        // answer="hellll";
+          
     }
 }
